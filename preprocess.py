f2b0ab7b87b49ace57482afa9f6de409c9376047
tsb46 <tsb46@miami.edu>
2025-09-19 11:43:20 -0400

    preprocess cli utility in base of repo

---
+++ preprocess.py
@@ -0,0 +1,31 @@
+"""
+Perform full preprocessing pipeline on selected subject
+"""
+
+import argparse
+
+from task_arousal.preprocess.pipeline import PreprocessingPipeline
+
+
+def main(subject):
+    pipeline = PreprocessingPipeline(subject)
+    # loop through tasks and preprocess
+    for task in pipeline.tasks:
+        print(f'Preprocessing task: {task} for subject: {subject}')
+        pipeline.preprocess(task=task, save_physio_figs=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Perform preprocessing pipeline on selected subject'
+    )
+    parser.add_argument(
+        '-s',
+        '--subject',
+        type=str,
+        required=True,
+        help='Subject to perform preprocessing pipeline',
+    )
+
+    args = parser.parse_args()
+    main(args.subject)
