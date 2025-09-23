6e3f986d3999d2c398e6e0ea233f7ca49f753a10
tsb46 <tsb46@miami.edu>
2025-09-22 20:17:36 -0400

    analysis cli script

---
+++ analyze.py
@@ -0,0 +1,354 @@
+"""
+Perform full analysis pipeline on selected subject
+"""
+
+import argparse
+import pickle
+
+import nibabel as nib
+import numpy as np
+
+from task_arousal.analysis.pca import PCA
+from task_arousal.analysis.complex_pca import ComplexPCA
+from task_arousal.analysis.glm import (
+    GLM,
+    GLMPhysio
+)
+from task_arousal.analysis.dlm import (
+    DistributedLagPhysioModel,
+    DistributedLagEventModel
+)
+from task_arousal.analysis.dataset import (
+    Dataset, 
+    DatasetLoad,
+    PINEL_CONDITIONS,
+    SIMON_CONDITIONS
+)
+
+# define output directory
+OUT_DIRECTORY = 'results'
+
+# physio signal labels
+PHYSIO_LABELS = [
+    'ppg_amplitude',
+    'heart_rate',
+    'resp_amp',
+    'resp_rate',
+    'endtidal_co2',
+    'endtidal_o2'
+]
+# define all tasks (exclude Motor task)
+TASKS = ['pinel', 'simon', 'rest', 'breathhold']
+
+# define tasks with event conditions
+TASKS_EVENT = ['pinel', 'simon']
+
+def main(subject, out_dir: str = OUT_DIRECTORY) -> None:
+    # initialize dataset loader
+    ds = Dataset(subject=subject)
+    # perform GLM analyses
+    print(f'Performing GLM analyses for subject {subject}')
+    # only perform GLM analyses for tasks with event conditions
+    for task in TASKS_EVENT:
+        print(f'Loading data for subject {subject}, task {task} for GLM analysis')
+        # load nifti images without conversion to 2d array for input to nilearn GLM analysis
+        data = ds.load_data(task=task, concatenate=False, convert_to_2d=False)
+        # perform GLM analysis with event regressors
+        _glm_event(data, ds, subject, task)
+        print(f'GLM analyses complete for subject {subject}, task {task}')
+
+    # free up memory by deleting data
+    del data
+    print(f'Performing PCA, Complex PCA, and DLM analyses for subject {subject}')
+    # perform analyses that require concatenated data
+    for task in TASKS:
+        print(f'Loading concatenated data for subject {subject}, task {task}')
+        data = ds.load_data(task=task, concatenate=True)
+        # perform PCA analysis
+        _pca(data, ds, subject, task)
+        print(f'PCA analysis complete for subject {subject}, task {task}')
+        # perform Complex PCA analysis
+        _cpca(data, ds, subject, task)
+        print(f'Complex PCA analysis complete for subject {subject}, task {task}')
+        # perform DLM analysis with physiological regressors
+        _dlm_physio(data, ds, subject, task)
+        print(f'DLM with physiological regressors analysis complete for subject {subject}, task {task}')
+    
+    # free up memory by deleting data
+    del data
+    # perform analyses that require non-concatenated data
+    # only perform DLM and GLM physio analyses for tasks with event conditions
+    for task in TASKS_EVENT:
+        print(f'Loading data for subject {subject}, task {task} for DLM with event and GLM with physio analyses')
+        data = ds.load_data(task=task, concatenate=False)
+        # perform DLM analysis with event regressors
+        _dlm_event(data, ds, subject, task)
+        print(f'DLM with event regressors analysis complete for subject {subject}, task {task}')
+        # perform GLM analysis with physiological regressors
+        _glm_physio(data, ds, subject, task)
+        print(f'GLM with physiological regressors analysis complete for subject {subject}, task {task}')
+    
+    
+
+def _cpca(data: DatasetLoad, ds: Dataset, subject: str, task: str) -> None:
+    """
+    Perform Complex PCA analysis on the given data and save results to files.
+
+    Parameters
+    ----------
+    data : DatasetLoad
+        Loaded dataset containing fMRI data and associated information
+    ds : Dataset
+        Dataset object for handling data operations
+    subject : str
+        Subject identifier
+    task : str
+        Task identifier
+    """
+    print(f'Performing Complex PCA on subject {subject}, task {task}')
+    # estimate Complex PCA with 10 components
+    cpca = ComplexPCA(n_components=10)
+    # run Complex PCA decomposition
+    cpca_results = cpca.decompose(data['fmri'][0])
+    # write amplitude and phase of PC loadings to nifti file
+    cpca_loadings_amp = ds.to_4d(cpca_results.loadings_amp.T)
+    cpca_loadings_phase = ds.to_4d(cpca_results.loadings_phase.T)
+    nib.save(cpca_loadings_amp, f'{OUT_DIRECTORY}/sub-{subject}_{task}_cpca_loadings_amp.nii.gz')
+    nib.save(cpca_loadings_phase, f'{OUT_DIRECTORY}/sub-{subject}_{task}_cpca_loadings_phase.nii.gz')
+    # write cpca metadata (including pc scores, exp var, etc.) to pickle file
+    pickle.dump(
+        cpca_results,
+        open(f'{OUT_DIRECTORY}/sub-{subject}_{task}_cpca_metadata.pkl', 'wb')
+    )
+
+
+def _dlm_event(data: DatasetLoad, ds: Dataset, subject: str, task: str) -> None:
+    """
+    Perform Distributed Lag Model (DLM) analysis with event regressors
+    on the given data and save results to files.
+
+    Parameters
+    ----------
+    data : DatasetLoad
+        Loaded dataset containing fMRI data and associated information
+    ds : Dataset
+        Dataset object for handling data operations
+    subject : str
+        Subject identifier
+    task : str
+        Task identifier
+    """
+    print(f'Performing DLM with event regressors on subject {subject}, task {task}')
+    if task  == 'pinel':
+        conditions = PINEL_CONDITIONS
+    elif task == 'simon':
+        conditions = SIMON_CONDITIONS
+    else:
+        raise ValueError(f'Task {task} not recognized for DLM event analysis')
+    
+    dlm = DistributedLagEventModel(
+        regressor_duration=20.0, n_knots=5, basis='cr'
+    )
+    dlm = dlm.fit(
+        event_dfs=data['events'],
+        outcome_data=data['fmri'] # type: ignore
+    )
+    # loop through conditions and write predicted functional time courses to nifti files
+    for condition in conditions:
+        dlm_eval = dlm.evaluate(trial=condition)
+        pred_func_img = ds.to_4d(dlm_eval.pred_outcome)
+        nib.save(pred_func_img, f'{OUT_DIRECTORY}/sub-{subject}_{task}_dlm_event_{condition}.nii.gz')
+
+        # write dlm metadata (including betas, t-stats, etc.) to pickle file
+        pickle.dump(
+            dlm_eval,
+            open(f'{OUT_DIRECTORY}/sub-{subject}_{task}_dlm_event_{condition}_metadata.pkl', 'wb')
+        )
+
+
+def _dlm_physio(data: DatasetLoad, ds: Dataset, subject: str, task: str) -> None:
+    """
+    Perform Distributed Lag Model (DLM) analysis with physiological regressors
+    on the given data and save results to files.
+
+    Parameters
+    ----------
+    data : DatasetLoad
+        Loaded dataset containing fMRI data and associated information
+    ds : Dataset
+        Dataset object for handling data operations
+    subject : str
+        Subject identifier
+    task : str
+        Task identifier
+    """
+    print(f'Performing DLM with physiological regressors on subject {subject}, task {task}')
+    # loop through physio signals 
+    for physio_label in PHYSIO_LABELS:
+        # estimate DLM with physiological regressors - using lags -3 to +8 TRs
+        dlm = DistributedLagPhysioModel(
+            neg_nlags=-3, nlags=8, n_knots=5, basis='cr'
+        )
+        dlm = dlm.fit(
+            X=data['physio'][0][physio_label].to_numpy().reshape(-1,1),
+            Y=data['fmri'][0] # type: ignore
+        )
+        # estimate functional time courses at each voxel to lagged physio signal
+        dlm_eval = dlm.evaluate()
+        # write predicted functional time courses to nifti file
+        pred_func_img = ds.to_4d(dlm_eval.pred_outcome)
+        nib.save(pred_func_img, f'{OUT_DIRECTORY}/sub-{subject}_{task}_dlm_physio_{physio_label}.nii.gz')
+
+        # write dlm metadata (including betas, t-stats, etc.) to pickle file
+        pickle.dump(
+            dlm_eval,
+            open(f'{OUT_DIRECTORY}/sub-{subject}_{task}_dlm_physio_{physio_label}_metadata.pkl', 'wb')
+        )
+
+
+def _glm_event(data: DatasetLoad, ds: Dataset, subject: str, task: str) -> None:
+    """
+    Perform General Linear Model (GLM) analysis with event regressors
+    on the given data and save results to files. Perform analysis for both
+    an 'spm' HRF model and a 'fir' HRF model.
+
+    Parameters
+    ----------
+    data : DatasetLoad
+        Loaded dataset containing fMRI data and associated information
+    ds : Dataset
+        Dataset object for handling data operations
+    subject : str
+        Subject identifier
+    task : str
+        Task identifier
+    """
+    print(f'Performing GLM with event regressors on subject {subject}, task {task}')
+    # perform GLM analysis with 'spm' HRF model
+    glm_spm = GLM(hrf='spm')
+    glm_spm_results = glm_spm.fit(event_df=data['events'], fmri=data['fmri'])
+    # write contrast maps to nifti files
+    for glm_map_name, glm_map in glm_spm_results.contrast_maps.items():
+        nib.save(glm_map, f'{OUT_DIRECTORY}/sub-{subject}_{task}_glm_hrf_contrast_{glm_map_name}.nii.gz')
+    # write glm metadata of HRF model (including contrasts, etc.) to pickle file
+    pickle.dump(
+        glm_spm_results,
+        open(f'{OUT_DIRECTORY}/sub-{subject}_{task}_glm_hrf_metadata.pkl', 'wb')
+    )
+
+    # perform GLM analysis with 'fir' HRF model
+    glm_fir = GLM(hrf='fir', fir_delays=np.arange(10)) # FIR with 10 TR delays
+    glm_fir_results = glm_fir.fit(event_df=data['events'], fmri=data['fmri'])
+    # write contrast maps to nifti files
+    for glm_map_name, glm_map in glm_fir_results.contrast_maps.items():
+        nib.save(glm_map, f'{OUT_DIRECTORY}/sub-{subject}_{task}_glm_fir_contrast_{glm_map_name}.nii.gz')
+    # write glm metadata of FIR model (including contrasts, etc.) to pickle file
+    pickle.dump(
+        glm_fir_results,
+        open(f'{OUT_DIRECTORY}/sub-{subject}_{task}_glm_fir_metadata.pkl', 'wb')
+    )
+
+
+def _glm_physio(data: DatasetLoad, ds: Dataset, subject: str, task: str) -> None:
+    """
+    Perform General Linear Model (GLM) analysis with physiological regressors
+    on the given data and save results to files. Analysis requires the choice
+    of lag on physio regressor to account for hemodynamic delay. Default is 
+    5.
+
+    Parameters
+    ----------
+    data : DatasetLoad
+        Loaded dataset containing fMRI data and associated information
+    ds : Dataset
+        Dataset object for handling data operations
+    subject : str
+        Subject identifier
+    task : str
+        Task identifier
+    """
+    print(f'Performing GLM with physiological regressors on subject {subject}, task {task}')
+    
+    if task  == 'pinel':
+        conditions = PINEL_CONDITIONS
+    elif task == 'simon':
+        conditions = SIMON_CONDITIONS
+    else:
+        raise ValueError(f'Task {task} not recognized for GLM physio analysis')
+    
+    # loop through physio signals
+    for physio_label in PHYSIO_LABELS:
+        print(f'Performing GLM with physiological regressor {physio_label}')
+        # perform GLM physio analysis with 'spm' HRF model
+        glm_physio = GLMPhysio(hrf='spm', physio_lag=5)
+        glm_physio.fit(
+            event_df=data['events'],
+            fmri=data['fmri'], # type: ignore
+            physio=[d[physio_label].to_numpy()[:,np.newaxis] for d in data['physio']]
+        )
+        # write contrast maps to nifti files
+        for contrast_name in conditions:
+            glm_eval = glm_physio.evaluate(contrast_name)
+            pred_func_img = ds.to_4d(glm_eval.pred_func)
+            nib.save(
+                pred_func_img, 
+                f'{OUT_DIRECTORY}/sub-{subject}_{task}_glm_hrf_{physio_label}_contrast_{contrast_name}.nii.gz'
+            )
+            # write glm metadata of HRF model (including contrasts, etc.) to pickle file
+            pickle.dump(
+                glm_eval,
+                open(f'{OUT_DIRECTORY}/sub-{subject}_{task}_glm_hrf_{physio_label}_contrast_{contrast_name}_metadata.pkl', 'wb')
+            )
+
+
+def _pca(data: DatasetLoad, ds: Dataset, subject: str, task: str) -> None:
+    """
+    Perform PCA decomposition on fMRI data and save results to files
+
+    Parameters
+    ----------
+    data : DatasetLoad
+        Loaded dataset containing fMRI data and associated information
+    ds : Dataset
+        Dataset object for handling data operations
+    subject : str
+        Subject identifier
+    task : str
+        Task identifier
+    """
+    print(f'Performing PCA on subject {subject}, task {task}')
+    # estimate PCA with 10 components
+    pca = PCA(n_components=10)
+    # run PCA decomposition
+    pca_results = pca.decompose(data['fmri'][0])
+    # write loadings to nifti file
+    pca_loadings = ds.to_4d(pca_results.loadings.T)
+    nib.save(pca_loadings, f'{OUT_DIRECTORY}/sub-{subject}_{task}_pca_loadings.nii.gz')
+    # write pca metadata (including pc scores, exp var, etc.) to pickle file
+    pickle.dump(
+        pca_results,
+        open(f'{OUT_DIRECTORY}/sub-{subject}_{task}_pca_metadata.pkl', 'wb')
+    )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Perform full analysis pipeline on selected subject'
+    )
+    parser.add_argument(
+        '-s',
+        '--subject',
+        type=str,
+        required=True,
+        help='Subject to perform analysis pipeline',
+    )
+    parser.add_argument(
+        '-o',
+        '--out_dir',
+        type=str,
+        required=False,
+        help="output directory to save analysis outputs",
+        default=OUT_DIRECTORY
+    )
+    args = parser.parse_args()
+    main(args.subject, args.out_dir)
