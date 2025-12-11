f6a4b274048d698e08b332162e236fc134624d34
tsb46 <tsb46@miami.edu>
2025-12-10 21:04:57 -0500

    remove datasethcp imports

--- task_arousal/dataset/__init__.py
+++ task_arousal/dataset/__init__.py
@@ -2,7 +2,12 @@
 Dataset module for loading preprocessed data for HCP and Euskalibur datasets.
 """
 
-from .dataset_hcp import DatasetHCPSubject  # noqa: F401
-from .dataset_euskalibur import DatasetEuskalibur  # noqa: F401
-from .dataset_group import GroupDataset  # noqa: F401
-from .dataset_utils import load_fmri, load_physio, to_4d  # noqa: F401
+from .dataset_euskalibur import DatasetEuskalibur
+from .dataset_group import GroupDataset
+
+all = [
+    'DatasetEuskalibur',
+    'GroupDataset'
+]
+
+
