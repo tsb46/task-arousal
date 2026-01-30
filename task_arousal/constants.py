74cdf510f815cafa1e1feaa9bc024cfd25c621ba
tsb46 <tsb46@miami.edu>
2026-01-29 20:45:57 -0500

    pan dataset constants and template

--- task_arousal/constants.py
+++ task_arousal/constants.py
@@ -1,6 +1,7 @@
 """
 Constants for the task arousal analysis.
 """
+
 import os
 from dotenv import load_dotenv
 
@@ -8,29 +9,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # path to data directory
-DATA_DIRECTORY_EUSKALIBUR = os.getenv('DATA_DIRECTORY_EUSKALIBUR', 'data/euskalibur')
-DATA_DIRECTORY_IBC = os.getenv('DATA_DIRECTORY_IBC', 'data/ibc')
-DATA_DIRECTORY_PAN = os.getenv('DATA_DIRECTORY_PAN', 'data/pan')
+DATA_DIRECTORY_EUSKALIBUR = os.getenv("DATA_DIRECTORY_EUSKALIBUR", "data/euskalibur")
+DATA_DIRECTORY_IBC = os.getenv("DATA_DIRECTORY_IBC", "data/ibc")
+DATA_DIRECTORY_PAN = os.getenv("DATA_DIRECTORY_PAN", "data/pan")
 # flag indicating if the data should be searched in the 'derivatives' directory
-IS_DERIVED = os.getenv('IS_DERIVED', 'false').lower() == 'true'
+IS_DERIVED = os.getenv("IS_DERIVED", "false").lower() == "true"
 
-# Brain mask
-MASK_EUSKALIBUR = 'templates/MNI152_T1_3mm_brain_mask_dil_euskalibur.nii.gz'
-MASK_IBC = 'templates/MNI152_T1_3mm_brain_mask_dil_ibc.nii.gz'
-MASK_PAN = 'templates/MNI152_T1_3mm_brain_mask_dil_pan.nii.gz'
+# Brain masks
+MASK_EUSKALIBUR = "templates/MNI152_T1_3mm_brain_mask_dil_euskalibur.nii.gz"
+MASK_IBC = "templates/MNI152_T1_3mm_brain_mask_dil_ibc.nii.gz"
+MASK_PAN = "templates/MNI152_T1_3mm_brain_mask_dil_pan.nii.gz"
+MASK_GM_PAN = "templates/MNI152_T1_3mm_gm_mask_pan.nii.gz"
 # TR (Repetition Time) in seconds
 TR_EUSKALIBUR = 1.5
 TR_PAN = 1.355
 
 
 # expected columns in event dataframe
-EVENT_COLUMNS = ['onset', 'duration', 'trial_type']
+EVENT_COLUMNS = ["onset", "duration", "trial_type"]
 
 
 ## Preprocessing parameters
 # Slice timing reference, between 0 and 1 (middle slice)
 SLICE_TIMING_REF = 0.5
-# Number of dummy volumes to drop 
+# Number of dummy volumes to drop
 DUMMY_VOLUMES = 10
 # High-pass filter cutoff frequency for fmri
 HIGHPASS = 0.01
@@ -39,6 +41,11 @@ FWHM_EUSKALIBUR = 4  # in mm
 FWHM_PAN = 4  # in mm
 FWHM_IBC = 5  # in mm
 # physio fields to extract from raw data
-PHYSIO_COLUMNS_EUSKALIBUR = ['respiratory_effort', 'cardiac', 'respiratory_CO2', 'respiratory_O2']
+PHYSIO_COLUMNS_EUSKALIBUR = [
+    "respiratory_effort",
+    "cardiac",
+    "respiratory_CO2",
+    "respiratory_O2",
+]
 # physiological resample frequency (in Hz)
 PHYSIO_RESAMPLE_F = 50
